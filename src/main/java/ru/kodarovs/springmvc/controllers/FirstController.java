9c357585584419099a9909328fb3127beb045e31
kodarov_salavat <kodarov@gmail.com>
2024-01-15 02:33:58 +0400

    20 - Spring MVC - Model - Thymeleaf

--- src/main/java/ru/kodarovs/springmvc/controllers/FirstController.java
+++ src/main/java/ru/kodarovs/springmvc/controllers/FirstController.java
@@ -2,6 +2,7 @@ package ru.kodarovs.springmvc.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,8 +12,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class FirstController {
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
-        System.out.println("Hello " + name + " " + surname);
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+        model.addAttribute("message","Hello " + name + " " + surname);
         return "first/hello";
     }
 
